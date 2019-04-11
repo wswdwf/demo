b3748631fa331708a6592dbda9ccbccc3bb450b6
wswdwf <877902430@qq.com>
2019-04-11 21:47:39 +0800

    demo

---
+++ src/com/fff/DateDemo.java
@@ -0,0 +1,19 @@
+package com.fff;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        demoDate();
+    }
+
+    public static void demoDate(){
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println(df.format(date));
+    }
+}
