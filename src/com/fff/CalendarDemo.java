7757a652c95f72de9e76c9c00197a9a5821af259
wswdwf <877902430@qq.com>
2019-04-12 10:27:58 +0800

    demo

---
+++ src/com/fff/CalendarDemo.java
@@ -0,0 +1,14 @@
+package com.fff;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarDemo {
+    public static void main(String[] args) {
+        Calendar c = new GregorianCalendar();
+        c.set(Calendar.YEAR,2000);
+        c.set(Calendar.MONTH,Calendar.OCTOBER);
+        c.set(Calendar.DATE,15);
+        System.out.println(c);   //不对具体时刻进行设置，会默认为当前时刻（几时几分几秒）
+    }
+}
